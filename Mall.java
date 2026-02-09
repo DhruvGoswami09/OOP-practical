9577734c1b4dfa087ed7da1c3d10da425c160de2
DhruvGoswami09 <goswamidhruvgiri321@gmail.com>
2026-02-09 20:37:25 +0530

    Mall.java

--- Mall.java
+++ Mall.java
@@ -1,19 +1,15 @@
 import java.util.*;
 
 class BillGenerator {
-    
-    // Regular Customer
     void generateBill(int total) {
         System.out.println("Final Bill Amount: " + total);
     }
-    
-    // Privileged Customer
+
     void generateBill(int total, int dis) {
         int finalAmt = total - dis;
         System.out.println("Final Bill Amount: " + finalAmt);
     }
-    
-    // Festive Offer
+
     void generateBill(int total, double per) {
         double disAmt = (total * per) / 100;
         double finalAmt = total - disAmt;
@@ -25,33 +21,35 @@ public class Mall {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         BillGenerator bg = new BillGenerator();
-        
+
         System.out.print("Enter Item Total: ");
         int t = sc.nextInt();
-        
+
         System.out.println("1. Regular Customer");
         System.out.println("2. Privileged Customer");
         System.out.println("3. Festive Offer");
         System.out.print("Select Type (1-3): ");
         int ch = sc.nextInt();
-        
-        // choice selection, we can use else if also
-                bg.generateBill(t);
-                break;
-                
-            case 1:
-                System.out.print("Enter Flat Discount (Rs): ");
-                int d = sc.nextInt();
-                bg.generateBill(t, d);
-                break;
-                
-            case 2:
+
+        switch(ch) {
+            case 3:
                 System.out.print("Enter Discount %: ");
                 double p = sc.nextDouble();
                 bg.generateBill(t, p);
                 break;
-                
+
+            case 2:
+                System.out.print("Enter Flat Discount (Rs): ");
+                int d = sc.nextInt();
+                bg.generateBill(t, d);
+                break;
+
+            case 1:
+                bg.generateBill(t);
+                break;
+
             default:
                 System.out.println("Invalid Choice");
         }
     }
+}
